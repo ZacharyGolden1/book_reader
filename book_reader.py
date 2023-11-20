98a186fc4259501e199e173d33e38664fe312a82
ZacharyGolden1 <golden13911@gmail.com>
2023-11-20 01:32:27 -0500

    added gitignore

--- book_reader.py
+++ book_reader.py
@@ -4,9 +4,12 @@ from tkinter_UI import SpeedReaderApp
 
 books = "/Users/golden/Desktop/Books/Personal/"
 grit = "Grit by Angela Duckworth.pdf"
+hgtg = "THE_HITCHHIKERS_GUIDE_TO_THE_GALAXY 2.pdf"
 
-text = extract_text(books + grit)
+text = extract_text(books + hgtg)
 words = re.split("[ \n]+", text)
 
-app = SpeedReaderApp(words, 0.10)
-app.run()
+                            
+app = SpeedReaderApp(words, 0.10, # delay (how many fractions of a second between words)
+                    0)# what word to start on
+app.run() 
